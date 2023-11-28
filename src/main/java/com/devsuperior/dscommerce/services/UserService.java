e4ea6a897851084d0948a05983d472dfef128c40
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2023-11-28 11:16:26 -0300

    Getting users login

--- src/main/java/com/devsuperior/dscommerce/services/UserService.java
+++ src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -1,12 +1,15 @@
 package com.devsuperior.dscommerce.services;
 
+import com.devsuperior.dscommerce.dto.UserDTO;
 import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -23,4 +26,18 @@ public class UserService implements UserDetailsService {
         return user;
     }
 
+    protected  User authenticated(){
+        try {
+            String username = SecurityContextHolder.getContext().getAuthentication().getName();
+            return repository.findByEmail(username);
+        }catch (Exception e){
+            throw new UsernameNotFoundException("Invalid User");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO getMe() {
+        User entity = authenticated();
+        return new UserDTO(entity);
+    }
 }
